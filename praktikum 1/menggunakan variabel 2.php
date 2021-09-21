dc8b927cdecc2df7a64fad509fc07526be146721
sepkhaayunda <sepkhaayunda05@gmail.com>
2021-09-21 09:11:25 +0700

    commit percobaan PHP 1-11

---
+++ praktikum 1/menggunakan variabel 2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba3-variabel2</title>
+</head>
+<body>
+    <?php 
+        $jalan = "Medan - B.Aceh";
+        $noRumah = 29;
+        $blok = "4C";
+        $jumlahPenghuni = 3;
+        echo "Hasil Variabel variabel jalan + variabel noRumah adalah ";
+        echo $alamat = $jalan . $noRumah;
+        echo "<br>";
+        echo "Hasil Variabel variabel noRumah + variabel jumlahPenghuni adalah ";
+        echo $Hasil = $noRumah + $jumlahPenghuni;
+        echo "<br>";
+        echo "Hasil Variabel variabel blok + variabel noRumah adalah ";
+        echo $Hasil = $Hasil + $noRumah;
+        echo "<br>";
+    ?>
+</body>
+</html>
---
+++ praktikum 1/operator penugasan.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba4-OperatorPenugasan</title>
+</head>
+<body>
+    <?php
+        $a = 3;
+        $b = 7;
+        $a += 5;
+        $b = ($c = 11) + 3;
+        echo "Nilai variabel a adalah = $a";
+        echo "<br>";
+        echo "Nilai variabel b adalah = $b";
+        echo "<br>";
+        echo "Nilai variabel c adalah = $c";
+    ?>
+</body>
+</html>
